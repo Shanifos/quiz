496361bd5b1c1428660dbfa17bc4646d3b4c6428
Shanifos <Shanifoshee@gmail.com>
2018-04-11 21:27:12 -0400

    Update Score.java

--- app/src/main/java/com/example/android/myquiz/Score.java
+++ app/src/main/java/com/example/android/myquiz/Score.java
@@ -12,30 +12,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Score extends AppCompatActivity {
-    public final static String Message_KEY = "com.example.android.myquiz.message.key";
+  
     String score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-      setContentView(R.layout.activity_score);
-
-      score = getIntent().getExtras().getString("score");
+        setContentView(R.layout.activity_score);
+        score = getIntent().getExtras().getString("score");
         TextView textView = (TextView)findViewById(R.id.score1);
         textView.setText(score);
-        //get the intent from the other page
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(Message_KEY);
-//        //Creating a new TextView on the fly
-//        TextView textView = new TextView(this);
-//       // textView.setTextColor(000);
-//        textView.setTextSize(45);
-//        textView.setText(message);
-//        setContentView(textView);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      //  setSupportActionBar(toolbar);
+      
         Button back = (Button)findViewById(R.id.back);
-       back.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
